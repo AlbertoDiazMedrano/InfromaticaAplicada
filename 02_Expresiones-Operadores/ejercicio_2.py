7659938bb86048ce3fae94f6883d5a2795933a00
AlbertoDiazMedrano <albertodiazmedrano@gmail.com>
2023-03-25 17:53:38 -0600

    Update

--- 02_Expresiones-Operadores/ejercicio_2.py
+++ 02_Expresiones-Operadores/ejercicio_2.py
@@ -1,230 +1,206 @@
-# Expresiones y Operadores
-
-## Expresiones.
-
-Las expresiones son combinaciones de constantes, variables, símbolos de operación, paréntesis y nombres de funciones especiales.
-
-Una expresión consta de operandos y operadores. Según sea el tipo de objetos que manipulan, las expresiones se clasifican en: 
-
-* aritméticas 
-* relacionales
-* lógicas
-* carácter
-
-El resultado de la expresión aritmética es de tipo numérico; el resultado de la expresión relacional y de una expresión lógica es de tipo lógico; el resultado de una expresión carácter es de tipo carácter.
-
-Las expresiones aritméticas son análogas a las fórmulas matemáticas. Las variables y constantes son numéricas (real o entera) y las operaciones son las aritméticas.
-
-* `+` - Este es el operador de suma y realiza la función de adicionar un operando al otro. 
-* `-` - Este es el operador de resta y realiza la función de sustraer un operando de otro.
-* `*` - Este es el operador de multiplicación y realiza la función de aumentar un operando en función de otro operando.
-* `/` - Este es el operador de división y realiza la función de seccionar un operando en función de otro operando.
-* `%` - Este es el operador de módulo y realiza la función de regresar el residuo de una división.
-* `**` - Este es el operador de exponencial y permite obtener la potencia de un operando en función de otro operando.
-
-A continuación se presentan algunos ejemplos de los operadores en código Python.
 
 ### Operador +.
 
 #python
+print("Resultados de sumas aritmeticas >INT<")
+print("-----------------------------------")
 a = 7 + 3
-print(a)
+print(type(a))                                       #10
 a = 5
 b = 3
 c = a + b
-print(c)
-print(3+4)
-#
+print(type(c))                                        #8
+print(type(3+4))                                      #7
+print("-----------------------------------")
 
-
-### Operador -.
-
-#python
+print("Resultados de resta aritmeticas >INT<")
+print("-----------------------------------")
 a = 6 - 2
-print(a)
+print(type(a))                                        #4
 a = 5
 b = 3
 c = a - b
-print(c)
-print(2-6)
-#
-
+print(type(c))                                        #2
+print(type(2-6))                                      #-4
+print("-----------------------------------")
 
 ### Operador *.
-
+print("Resultados de multiplicacion aritmeticas CLASE <INT>")
 #python
+print("-----------------------------------")
 a = 3 * 4
-print(a)
+print(a)            #12
 a = 6
 b = 3
-c = a * b
+c = a * b               #18
 print(c)
-print(5*7)
-#
+print(5*7)              #35
+print("-----------------------------------")
 
-
+print("Resultados de dicivion aritmeticas   clase float")
 ### Operador /.
-
+print("-----------------------------------")
 #python
 a = 6 / 2
-print(a)
+print(type(a))            # 3
 a = 5
 b = 3
 c = a / b
-print(c)
-print(10/3)
+print(type(c))            #1.666
+print(type(10/3))         #3.3333
 #
-
+print("-----------------------------------")
 ### Operador %.
 
+print("Resultados porcentuales clase int ")
+print("-----------------------------------")
 #python
 a = 8 % 4
-print(a)
+print(type(a))            #0
 a = 9
 b = 2
 c = a % b
-print(c)
-print(6%3)
+print(type(c))            #1
+print(type(6%3))          #0
 #
-
+print("-----------------------------------")
 ### Operador **.
 
+
+print("Resultados de potencias  clase int")
 #python
+print("-----------------------------------")
 a = 3 ** 3
-print(a)
+print(type(a))            #27
 a = 2
 b = 4
 c = a ** b
-print(c)
-print(4**3)
-#
+print(type(c))            #16
+print(type(4**3))         #64
+print("-----------------------------------")
 
-Expresiones relacionales
-
-* `<` - Este es el operador de menor que, y se utiliza en las expresiones relacionales para saber si un operando es menor a otro operando.
-* `>` - Este es el operador de mayor que, y se utiliza en las expresiones relacionales para saber si un operando es mayor a otro operando.
-* `==` - Este es el operador de igual que, y se utiliza en las expresiones relacionales para saber si un operando es igual a otro operando.
-* `!=` - Este es el operador desigual que, y se utiliza en las expresiones relacionales para saber si un operando es diferente a otro operando.
-* `<=` - Este es el operador de menor igual que, y se utiliza en las expresiones relacionales para saber si un operando es menor o igual a otro operando.
-* `>=` Este es el operador de mayor igual que, y se utiliza en las expresiones relacionales para saber si un operando es mayor igual a otro operando.
-
-NOTA: Como resultado de la aplicación de las expresiones relacionales, se obtendrá como resultado una salida booleana, es decir `TRUE` o `FALSE`.
-
-A continuación se presentan algunos ejemplos de las expresiones relacionales en código Python.
-
-### Operador <.
+#Operador <.
 
 #python
+print("Resultados de menor que   clase bool")
+print("-----------------------------------")
 a = 3 < 3
-print(a)
+print(type(a))            #FALSE
 a = 2
 b = 4
 c = a < b
-print(c)
-print(4<3)
+print(type(c))            #TRUE
+print(type(4<3))          #FALSE
 #
-
+print("-----------------------------------")
 ### Operador >.
-
+print("Resultados de mayor que  clase bool")
 #python
+print("-----------------------------------")
 a = 4 > 2
-print(a)
+print(type(a))            #TRUE
 a = 5
 b = 7
 c = a > b
-print(c)
-print(9>2)
+print(type(c))            #FALSE
+print(type(9>2))          #FALSE
 #
-
+print("-----------------------------------")
 ### Operador ==.
 
 #python
+print("Resultados de igual a  clase bool")
+print("-----------------------------------")
 a = 5 == 5
-print(a)
+print(type(a))            #TRUE
 a = 6
 b = 9
 c = a == b
-print(c)
-print(6==2)
+print(type(c))            #TRUE
+print(type(6==2))         #FALSE
 #
-
-### Operador !=.
+print("-----------------------------------")
+# Operador !=.
 
 #python
+print("Resultados de diferente a clase bool")
+print("-----------------------------------")
 a = 4 != 2
-print(a)
+print(type(a))            #FALSE
 a = 5
 b = 3
 c = a != b
-print(c)
-print(8!=8)
+print(type(c))            #FALSE
+print(type(8!=8))         #FALSE
 #
-
+print("-----------------------------------")
 ### Operador <=.
 
-
+print("Resultados de menor o igual  clase bool")
 #python
+print("-----------------------------------")
 a = 5 <= 3
-print(a)
+print(type(a))            #FALSE
 x = 7
 y = 5
 z = x <= y
-print(z)
-print(9<=4)
+print(type(z))            #FALSE
+print(type(9<=4))         #FALSE
 #
-
+print("-----------------------------------")
 ### Operador >=.
 
 #python
+print("Resultados de mayor o igual a clase bool")
+print("-----------------------------------")
 a = 2 >= 8
-print(a)
+print(type(a))        #FALSE
 a = 3
 b = 4
 c = a >= b
-print(c)
-print(7>=3)
+print(type(c))        #FALSE
+print(type(7>=3))     #TRUE
 #
-
+print("-----------------------------------")
 
 ## Expresiones lógicas.
 
 
-* `not` - Este es el operador lógico para realizar una negación, si el operando no se encuentra en el otro operando, la expresión se cumple y resulta en True, caso contrario, si la expresión no se cumple el resultado es False.
-* `and` - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
-* `or` - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
-
-### Operador and.
-
 #python
-print(4-1==3 and 5>6)
-print(6+7 > 11 and 3==3)
+print("Resultados de la compuerta AND  clase bool")
+print("-----------------------------------")
+print(type(4-1==3 and 5>6))       #FALSE
+print(type(6+7 > 11 and 3==3))    #TRUE
 #
-
-### Operador or.
-
+print("-----------------------------------")
+# Operador or.
+print("Resultados de la compuerta OR clase bool")
 #python
-print(4-1==3 or 5>6)
-print(6+7 > 11 or 3==3)
+print("-----------------------------------")
+print(type(4-1==3 or 5>6))        #TRUE
+print(type(6+7 > 11 or 3==3))     #TRUE
 #
-
+print("-----------------------------------")
 ### Operador not.
 
 #python
-print(not 5>6)
-print(not 5>4)
+print("Resultados de la compuerta NOT clase bool")
+print("-----------------------------------")
+print(type(not 5>6))      #true
+print(type(not 5>4))      #FALSE
 #
-
+print("-----------------------------------")
 ## Expresiones de carácter
 
-A diferencia de las demás expresiones no existe un operador estático sino una búsqueda de secuencias, números o caracteres dentro de una variable.
+#A diferencia de las demás expresiones no existe un operador estático sino una búsqueda de secuencias, números o caracteres dentro de una variable.
 
-Estas expresiones de búsqueda comúnmente llamadas expresiones regulares, sirven para captar ciertos elementos o patrones dentro de un valor.
-
-Ejemplo:
+#Estas expresiones de búsqueda comúnmente llamadas expresiones regulares, sirven para captar ciertos elementos o patrones dentro de un valor
 
 #python
+print("-----------------------------------")
 import re
 frase = "Tengo 2 hijos que tienen 15 y 11 años"
 patron = '[0-9]+' #Esta es una expresión regular
-re.findall(patron, frase)
+re.findall(type(patron, frase))
 #
+print("-----------------------------------")
