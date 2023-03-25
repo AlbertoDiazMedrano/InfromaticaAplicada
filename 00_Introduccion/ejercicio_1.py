fd2e86f8e8fd45bb45e0623bab3aac08129c1bf7
AlbertoDiazMedrano <albertodiazmedrano@gmail.com>
2023-03-25 16:11:00 -0600

    Update example

--- 00_Introduccion/ejercicio_1.py
+++ 00_Introduccion/ejercicio_1.py
@@ -3,29 +3,29 @@
 # Introducción
 # Una expresión puede ser una operación aritmetica
 
-print(6 + 9)                       # 15
-print(3 - 1)                       # 
-print(1 * 1)                       # 
-print(8 / 5)                       # 
-print(2 ** 4)                      # 
-print(8 % 3)                       # 
-print(9 // 4)                      # 
-print(15<6)                        # 
-print(2.5 * 1.5 * 3)               # 
-print(2j + 2 + 3j + 5)             # 
-print('Hola mundo')                # 
-print(['Mar', 2, 5])               # 
-print({'Nombre':'Tu nombre'})      #
-print({'Algo', 2.67, 9.5})         # 
-print(('Lag', 6.8, 7.5, 9))        # 
+print(11 + 9)                       # 20
+print(200 - 1)                       #199 
+print(2 * 890)                       # 1780
+print(1500 / 5)                       # 300.0
+print(3 ** 4)                      # 81
+print(16 % 3)                       # 1
+print(10 // 6)                      # 1
+print(150<1)                        # False
+print(10.5 * 100.5 * 3)               # 3165.75
+print(2j + 2 + 1j + 10)             # (12+3j)
+print('prueba')                # prueba
+print(['Mar', 1, 6])               # ['Mar', 1, 6]
+print({'Nombre':'Alberto Diaz Medrano'})      # Nombre: Alberto Diaz Medrano
+print({'Algo', 2.67, 9.5})         # {'Algo', 9.5, 2.67}
+print(('Lag', 7.8, 6.5, 9))        # ('Lag',7.8, 6.5, 9)
 
 # Tipos de datos
 
-print(type(15<6))                       # <class 'bool'>
-print(type(2.5 * 1.5 * 3))              #
-print(type(2j + 2 + 3j + 5))            #
-print(type('Hola mundo'))               # 
-print(type(['Mar', 2, 5]))            # 
-print(type({'Nombre':'Tu nombre'}))     # 
-print(type({'Algo', 2.67, 9.5}))        # 
-print(type(('Lag', 6.8, 7.5, 9)))       # 
+print(type(56<1))                       # <'bool'>
+print(type(20.5 * 11.5 * 3))              #<'float'>
+print(type(2j - 2 + 30j + 5))            #<'complex'>
+print(type('Hola '))               # <'str'>
+print(type(['Mar', 1, 5]))            # <'list'>
+print(type({'Nombre':'Alberto'}))     # <'dict'>
+print(type({'Algo', 2.67, 0.5}))        # <'set'>
+print(type(('Lag', 6.8, 7.5, 1)))       # <'tuple'>
